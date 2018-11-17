9c7b14ac70d1c75112c3bd701a749592bbf4447c
NdibeRaymond <40905613+NdibeRaymond@users.noreply.github.com>
2018-11-17 23:34:22 +0100

    Add files via upload

--- pingle/transparent_to_solid_navbar.js
+++ pingle/transparent_to_solid_navbar.js
@@ -37,3 +37,44 @@ $(".navbar-toggle.collapsed").on("click", function(){
       document.querySelector(".panel-title").textContent = "Click to see more votes"
       // checkScroll();
     };})
+
+
+
+// 786
+
+
+// $(window).on('resize', function(){
+//   console.log($(window).width());
+//   var small_search = document.createTextNode("<img src='project_images\search-magnifier-interface-symbol.png' alt=''>");
+//   var big_search = document.createTextNode("<div class='input-group' style='width:40vw;margin-top:10px;'><input type='text' class='form-control' placeholder='Search for anything'><span class='input-group-btn'><button class='btn btn-success' type='button' style='padding:8px;'><img src=' project_images\search-magnifier-interface-symbol (1).png' alt=''></button></span></div>");
+//   if($(window).width()<786){
+//     var list = document.createElement("li");
+//     list.appendChild(small_search);
+//     var item = document.getElementById("search");
+//     item.replaceChild(list,item.childNodes[0]);
+    // console.log(document.querySelector("#search"));
+    // document.querySelector("#search").replaceWith("<img id = 'search' src='project_images\search-magnifier-interface-symbol.png' alt=''>");
+  // }else{
+  //   var list = document.createElement("li");
+  //   list.appendChild(big_search);
+  //   var item = document.getElementById("search");
+  //   item.replaceChild(list,item.childNodes[0]);
+    // console.log(document.querySelector("#search"));
+    // document.querySelector("#search").replaceWith(<div id = 'search' class='input-group' style='width:40vw;margin-top:10px;'><input type='text' class='form-control' placeholder='Search for anything'><span class='input-group-btn'><button class='btn btn-success' type='button' style='padding:8px;'><img src=' project_images\search-magnifier-interface-symbol (1).png' alt=''></button></span></div>);
+// }
+//     })
+
+
+
+   //  var elmnt = document.createElement("li");
+   //     var textnode = document.createTextNode("Water");
+   //     elmnt.appendChild(textnode);
+   //
+   //     var item = document.getElementById("myList");
+   //     item.replaceChild(elmnt, item.childNodes[0]);
+   // }
+
+    // var textnode = document.createTextNode("Water");
+    //     var item = document.getElementById("myList").childNodes[0];
+    //     item.replaceChild(textnode, item.childNodes[0]);
+    // }
